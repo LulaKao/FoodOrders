5b0cb3652f58e07b0794b71a376e1ec0a93462fa
LulaKao <windandsprings@gmail.com>
2021-01-27 12:02:10 +0800

    create orderList ( mutableListOf<Order> ) and add orders in orderList

--- app/src/main/java/com/quarterlife/foodorders/MainActivity.kt
+++ app/src/main/java/com/quarterlife/foodorders/MainActivity.kt
@@ -8,21 +8,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // create orderList
+        val orderList = mutableListOf<Order>()
+
         // create order1
         val order1 = Order(1)
         order1.addItem(Noodles()) // add item
-        order1.print() // print item
+        orderList.add(order1) // add order1 in orderList
 
         // create order2
         val order2 = Order(2)
         order2.addItem(Noodles()) // add item
         order2.addItem(Vegetables()) // add item
-        order2.print() // print item
+        orderList.add(order2) // add order2 in orderList
 
         // create order3
         val order3 = Order(3)
         val items = listOf(Noodles(), Vegetables("Carrots", "Beans", "Celery")) // create list of items
         order3.addAll(items) // add all items
-        order3.print() // print item
+        orderList.add(order3) // add order3 in orderList
+
+        // print each order in orderList
+        for(order in orderList){
+            order.print()
+        }
     }
 }
